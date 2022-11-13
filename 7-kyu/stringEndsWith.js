2cf3356c85e9e04600bd6c9663c73aa52893992c
Ruben <titorenko.ruben@gmail.com>
2022-11-13 20:20:04 +0300

    Made 3 tasks-7 kyu

---
+++ 7-kyu/stringEndsWith.js
@@ -0,0 +1,18 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+console.log(solution('abcde', 'cde'))
+
+function solution(str, ending){
+	return str.endsWith(ending, str.length)
+  }
+
+  //or
+
+  function solution(str, ending){
+	return str.endsWith(ending);
+  }
